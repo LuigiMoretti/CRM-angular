c1b1894251937397b9757efe86f01dcd58f4c1f2
Luigi Moretti <luigimoretti98@gmail.com>
2022-03-17 10:20:27 +0100

    primo commit

---
+++ src/app/auth/token.interceptor.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+import { switchMap, take } from 'rxjs/operators';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+  constructor(private authSrv: AuthService) {}
+
+  intercept(
+    request: HttpRequest<unknown>,
+    next: HttpHandler
+  ): Observable<HttpEvent<unknown>> {
+    return this.authSrv.user$.pipe(
+      take(1),
+      switchMap((user) => {
+        // if (!user) {
+        //   return next.handle(request);
+        // }
+        const newReq: HttpRequest<any> = request.clone({
+          headers: request.headers
+            .set(
+              'Authorization',
+              'Bearer ' +
+                'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTY0NzUwNzE5OCwiZXhwIjoxNjQ4MzcxMTk4fQ.hDmA5CnOrvWrGOq_cOiMaptYKXUvU186b5BoxNRqjLrDfpByPrdIacV0GFkBA_U7lWVqK4LekBVXXAskE_ft6Q'
+            )
+            .set('X-TENANT-ID', 'fe_0721a'),
+        });
+
+        return next.handle(newReq);
+      })
+    );
+  }
+}
---
+++ src/app/pages/clienti/clienti.page.ts
@@ -0,0 +1,103 @@
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente';
+import { Fattura } from 'src/app/models/fattura';
+import { ClientiService } from './clienti.service';
+@Component({
+  templateUrl: './clienti.page.html',
+  styleUrls: ['./clienti.page.scss'],
+})
+export class ClientiPage implements OnInit {
+  clienti: Cliente[] | undefined;
+  pagina = 0;
+  isLoading = false;
+  booleanoFiltro = false;
+  fatturatoValore = false;
+  fattureCliente: Fattura[] | undefined;
+  isLoadingModale = false;
+  idDaEliminare: number | undefined;
+
+  constructor(private clSrv: ClientiService) {}
+
+  ngOnInit(): void {
+    this.getClienti();
+  }
+
+  getClienti() {
+    this.clSrv
+      .getClienti(this.pagina)
+      .subscribe((response) => (this.clienti = response.content));
+  }
+
+  async onRemoveCliente() {
+    this.clSrv.eliminaFattureCLiente(this.idDaEliminare!).subscribe((response) => {
+      this.clSrv.removeCliente(this.idDaEliminare!);
+  setTimeout(() => {
+    this.getClienti();
+  }, 200);
+    });
+  }
+
+  cambiaPagina(param: string) {
+    if (param == '+') {
+      this.pagina++;
+    } else if (param == '-') {
+      this.pagina--;
+    }
+    this.getClienti();
+  }
+
+  getFiltro() {
+    const filtro = (<HTMLInputElement>document.getElementById('filtro')).value;
+    if (filtro == 'fatturatoannuale') {
+      this.fatturatoValore = true;
+    } else {
+      this.fatturatoValore = false;
+    }
+  }
+
+  filtraFatturato() {
+    this.isLoading = true;
+    const filtro = (<HTMLInputElement>document.getElementById('filtro')).value;
+    const valoreFiltro1 = (<HTMLInputElement>document.getElementById('valore1'))
+      .value;
+    const valoreFiltro2 = (<HTMLInputElement>document.getElementById('valore2'))
+      .value;
+    this.clSrv
+      .getClientiFiltratiFatturato(filtro, valoreFiltro1, valoreFiltro2)
+      .subscribe((res) => {
+        this.clienti = res.content;
+        this.isLoading = false;
+      });
+  }
+
+  filtra() {
+    this.booleanoFiltro = true;
+    this.isLoading = true;
+    const filtro = (<HTMLInputElement>document.getElementById('filtro')).value;
+    const valoreFiltro = (<HTMLInputElement>document.getElementById('valore'))
+      .value;
+
+    this.clSrv
+      .getClientiFiltrati(filtro, valoreFiltro, this.pagina)
+      .subscribe((res) => {
+        if (filtro == 'id') {
+          this.clienti = [];
+          this.clienti.push(res);
+          this.isLoading = false;
+        } else {
+          this.clienti = res.content;
+          this.isLoading = false;
+        }
+      });
+  }
+
+  getFattureCliente(id: number) {
+    this.isLoadingModale = true;
+    this.clSrv.getAllFattureCliente(id).subscribe((res) => {
+      this.fattureCliente = res.content;
+      this.isLoadingModale = false;
+      this.idDaEliminare = id;
+      console.log(this.idDaEliminare)
+    });
+  }
+}
